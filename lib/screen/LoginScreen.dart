272ba3ad2a78a679a228d18f5c1704402b55db64
Vasyilla Kautsar Al Fitra Yudha <80631934+vasyillakafy@users.noreply.github.com>
2022-05-12 22:00:10 +0700

    update login

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sejuta/config/palete.dart';
+import 'package:sejuta/config/constants.dart';
+import 'package:sejuta/screen/register.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -37,6 +39,9 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
+                        SizedBox(
+                          height: 40,
+                        ),
                         Center(
                           child: Container(
                             height: 166,
@@ -44,35 +49,27 @@ class _LoginScreenState extends State<LoginScreen> {
                             child: Image.asset('images/iLogin.png'),
                           ),
                         ),
-                        // SizedBox(
-                        //   height: 100,
-                        // ),
+                        SizedBox(
+                          height: 20,
+                        ),
                         Text(
                           "Masuk",
-                          style: TextStyle(
-                            color: color_font,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          style:
+                              defaultText.headline1?.apply(color: color_font),
                         ),
                         Text(
                           "Masuk untuk melakukan donasi",
-                          style: TextStyle(
-                              color: color_font,
-                              fontSize: 20,
-                              fontWeight: FontWeight.normal),
+                          style:
+                              defaultText.headline6?.apply(color: color_font),
                         ),
                         SizedBox(
-                          height: 60,
+                          height: 40,
                         ),
                         buildEmail(),
                         SizedBox(
-                          height: 50,
+                          height: 30,
                         ),
                         buildPassword(),
-                        SizedBox(
-                          height: 50,
-                        ),
                         // Row(
                         //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         //   children: [
@@ -87,6 +84,14 @@ class _LoginScreenState extends State<LoginScreen> {
                         SizedBox(
                           height: 30,
                         ),
+                        Text(
+                          "atau masuk dengan",
+                          style:
+                              defaultText.headline6?.apply(color: color_font),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
@@ -96,12 +101,26 @@ class _LoginScreenState extends State<LoginScreen> {
                           ],
                         ),
                         SizedBox(
-                          height: 60,
+                          height: 30,
                         ),
-                        Text(
-                          "الشروط والاحكام",
-                          style: TextStyle(color: color_font, fontSize: 10),
-                        )
+                        Text("Jika belum punya akun,",
+                            style: defaultText.subtitle1
+                                ?.apply(color: color_font)),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => register()));
+                            },
+                            child: Text(
+                              "klik disini",
+                              style: defaultText.subtitle2
+                                  ?.apply(color: color_font),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
                       ],
                     ),
                   ),
@@ -115,92 +134,100 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget buildEmail() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Email",
-          style: TextStyle(
-              color: color_font, fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 60,
-          alignment: Alignment.centerLeft,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: color_font,
-                  offset: Offset(0, 2),
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(color: color_font),
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(top: 14),
-                prefixIcon: Icon(
-                  Icons.email,
-                  color: color_font,
-                ),
-                hintText: 'Masukkan email/no telepon',
-                hintStyle: TextStyle(color: color_font)),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Email",
+            style: TextStyle(
+                color: color_font, fontSize: 16, fontWeight: FontWeight.bold),
           ),
-        ),
-      ],
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 60,
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: color_font,
+                    blurRadius: 6,
+                    offset: Offset(0, 2),
+                  )
+                ]),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: color_font),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: color_font,
+                  ),
+                  hintText: 'Masukkan email/no telepon',
+                  hintStyle: defaultText.subtitle1?.apply(color: color_font)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   Widget buildPassword() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Password",
-          style: TextStyle(
-              color: color_font, fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(color: color_font, blurRadius: 6, offset: Offset(0, 2))
-            ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Password",
+            style: TextStyle(
+                color: color_font, fontSize: 16, fontWeight: FontWeight.bold),
           ),
-          height: 60,
-          child: TextField(
-            obscureText: sec,
-            style: TextStyle(color: color_font),
-            decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      sec = !sec;
-                    });
-                  },
-                  icon: sec ? visableoff : visable,
-                ),
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(top: 14),
-                prefixIcon: Icon(
-                  Icons.vpn_key,
-                  color: color_font,
-                ),
-                hintText: "Password",
-                hintStyle: TextStyle(color: color_font)),
+          SizedBox(
+            height: 10,
           ),
-        )
-      ],
+          Container(
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: color_font, blurRadius: 6, offset: Offset(0, 2))
+              ],
+            ),
+            height: 60,
+            child: TextField(
+              obscureText: sec,
+              style: TextStyle(color: color_font),
+              decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        sec = !sec;
+                      });
+                    },
+                    icon: sec ? visableoff : visable,
+                  ),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(
+                    Icons.vpn_key,
+                    color: color_font,
+                  ),
+                  hintText: "Password",
+                  hintStyle: defaultText.subtitle1?.apply(color: color_font)),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -243,21 +270,20 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget buildLoginButton() {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 25),
+      padding: const EdgeInsets.symmetric(horizontal: 35),
       child: Container(
         width: double.infinity,
         child: RaisedButton(
           onPressed: () {},
           elevation: 5,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
+            borderRadius: BorderRadius.circular(10),
           ),
           color: color_font,
-          padding: EdgeInsets.all(30),
+          padding: EdgeInsets.all(25),
           child: Text(
             "Masuk",
-            style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+            style: defaultText.headline3?.apply(color: Colors.white),
           ),
         ),
       ),
