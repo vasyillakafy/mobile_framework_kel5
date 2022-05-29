51b5bc294b0902024e7591c49634ba4941b36bac
Vasyilla Kautsar Al Fitra Yudha <80631934+vasyillakafy@users.noreply.github.com>
2022-05-29 21:03:37 +0700

    upload bantuan dan tentang

---
+++ lib/screen/bantuan.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class bantuan extends StatefulWidget {
+  @override
+  State<bantuan> createState() => _bantuanState();
+}
+
+class _bantuanState extends State<bantuan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("bantuan"),
+      ),
+    );
+  }
+}
---
+++ lib/screen/tentang.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class tentang extends StatefulWidget {
+  @override
+  State<tentang> createState() => _tentangState();
+}
+
+class _tentangState extends State<tentang> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("tentang"),
+      ),
+    );
+  }
+}
