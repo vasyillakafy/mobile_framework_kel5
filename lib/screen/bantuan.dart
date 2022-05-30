15554d5c62071c8c6f4a357e763540f82d0d2765
Vasyilla Kautsar Al Fitra Yudha <80631934+vasyillakafy@users.noreply.github.com>
2022-05-30 21:39:26 +0700

    Update bantuan

--- lib/screen/bantuan.dart
+++ lib/screen/bantuan.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:sejuta/config/constants.dart';
+import 'package:sejuta/config/palete.dart';
 
 class bantuan extends StatefulWidget {
   @override
@@ -9,9 +13,16 @@ class _bantuanState extends State<bantuan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("bantuan"),
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new),
+        iconTheme: IconThemeData(color: color_font),
+        title: Text(
+          "Bantuan",
+          style: defaultText.button?.apply(color: color_font),
+        ),
+        backgroundColor: color_background,
       ),
+      backgroundColor: color_background,
     );
   }
 }
